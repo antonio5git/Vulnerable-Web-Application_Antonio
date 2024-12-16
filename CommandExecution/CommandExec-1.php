1461a9f43367b7c566c3c0b32b4bcd3dccce7605
antoniogm56 <antoniogm56@educastur.es>
2024-12-16 16:45:43 +0100

    Update CommandExec-1.php

--- CommandExecution/CommandExec-1.php
+++ CommandExecution/CommandExec-1.php
@@ -22,8 +22,10 @@
   <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px" align="center">
     <?php
     if(isset($_GET["username"])){
-      echo shell_exec($_GET["username"]);
-      if($_GET["username"] == "Admin" && $_GET["password"] == "ufoundmypassword")
+      $user = $_GET["username"];
+      $password = $_GET["password"];
+      echo shell_exec($user);
+      if($user == "Admin" && $password == "ufoundmypassword")
         echo "WELLDONE";
     }
 
