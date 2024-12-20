1866f7e555e3168e112433e29b4e7eaeb8cea0fb
antoniogm56 <antoniogm56@educastur.es>
2024-12-20 10:35:09 +0100

    Update XSS_level5.php

--- XSS/XSS_level5.php
+++ XSS/XSS_level5.php
@@ -20,7 +20,7 @@
 <?php 
 if (isset($_GET["username"])) {
     $user = str_replace("<", "", $_GET["username"]);
-    echo "Your name is "."$user";
+    echo htmlspecialchars("Your name is "."$user");
 }
  ?>
 
